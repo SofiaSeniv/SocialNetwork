a30f2a499cdc4e56da4c9d414cdab4854d6c846a
SofiaSeniv <90135240+SofiaSeniv@users.noreply.github.com>
2023-11-28 20:33:44 +0200

    Add files via upload

---
+++ DynamoDb/Program.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.DynamoDBv2;
+using DAL;
+using Neo4jClient;
+using SocialNetwork.UI;
+
+namespace SocialNetwork
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Menu menu = new Menu();
+            //menu.ShowMenu();
+
+
+            //var client = new GraphClient(new Uri("http://localhost:7474"), "neo4j", "12345678");
+            //client.ConnectAsync().Wait();
+
+            //var socialRepository = new SocialNetworkRepositoryNeo(client);
+
+            var repository = new SocialNetworkRepositoryDynamo();
+            var consoleApp = new UIDynamo(repository);
+            await consoleApp.Run();
+        }
+    }
+}
