d7687d770d35953613c83c448a2d1f53a759665c
SofiaSeniv <90135240+SofiaSeniv@users.noreply.github.com>
2023-11-28 20:35:52 +0200

    Add files via upload

--- DynamoDb/SocialNetworkRepositoryDynamo.cs
+++ DynamoDb/SocialNetworkRepositoryDynamo.cs
@@ -1,12 +1,123 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 
 namespace DAL
 {
-    internal class SocialNetworkRepositoryDynamo
+    public class SocialNetworkRepositoryDynamo
     {
+
+        AmazonDynamoDBClient _client;
+        private const string TableName = "SocialMediaPosts";
+
+
+        public SocialNetworkRepositoryDynamo()
+        {
+            AmazonDynamoDBConfig ddbConfig = new AmazonDynamoDBConfig();
+            ddbConfig.ServiceURL = "http://localhost:8000";
+            _client = new AmazonDynamoDBClient(ddbConfig); 
+        }
+
+
+        public async Task CreateSocialMediaTable()
+        {
+            CreateTableRequest createTableRequest = new CreateTableRequest
+            {
+                TableName = TableName,
+                AttributeDefinitions = new List<AttributeDefinition>
+            {
+                new AttributeDefinition("PostId", ScalarAttributeType.N),
+                new AttributeDefinition("CommentId", ScalarAttributeType.N),
+                new AttributeDefinition("ModifiedDateTime", ScalarAttributeType.S) // Sort key for sorting comments
+            },
+                KeySchema = new List<KeySchemaElement>
+            {
+                new KeySchemaElement("PostId", KeyType.HASH), // Partition key
+                new KeySchemaElement("CommentId", KeyType.RANGE) // Sort key for comments
+            },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 5,
+                    WriteCapacityUnits = 5
+                },
+                GlobalSecondaryIndexes = new List<GlobalSecondaryIndex>
+            {
+                new GlobalSecondaryIndex
+                {
+                    IndexName = "PostModifiedDateTimeIndex",
+                    KeySchema = new List<KeySchemaElement>
+                    {
+                        new KeySchemaElement("PostId", KeyType.HASH),
+                        new KeySchemaElement("ModifiedDateTime", KeyType.RANGE)
+                    },
+                    Projection = new Projection { ProjectionType = ProjectionType.ALL },
+                    ProvisionedThroughput = new ProvisionedThroughput
+                    {
+                        ReadCapacityUnits = 5,
+                        WriteCapacityUnits = 5
+                    }
+                }
+            }
+            };
+
+            CreateTableResponse createTableResponse = await _client.CreateTableAsync(createTableRequest);
+        }
+
+        public async Task EditPost(string postId, string newContent)
+        {
+            UpdateItemRequest updatePostRequest = new UpdateItemRequest
+            {
+                TableName = TableName,
+                Key = new Dictionary<string, AttributeValue>
+            {
+                { "PostId", new AttributeValue { N = postId } }
+            },
+                UpdateExpression = "SET Content = :content",
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+            {
+                { ":content", new AttributeValue { S = newContent } }
+            }
+            };
+
+            UpdateItemResponse updatePostResponse = await _client.UpdateItemAsync(updatePostRequest);
+        }
+
+        public async Task EditComment(string postId, string commentId, string newCommentText)
+        {
+            UpdateItemRequest updateCommentRequest = new UpdateItemRequest
+            {
+                TableName = TableName,
+                Key = new Dictionary<string, AttributeValue>
+            {
+                { "PostId", new AttributeValue { N = postId } },
+                { "CommentId", new AttributeValue { N = commentId } }
+            },
+                UpdateExpression = "SET CommentText = :commentText",
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+            {
+                { ":commentText", new AttributeValue { S = newCommentText } }
+            }
+            };
+
+            UpdateItemResponse updateCommentResponse = await _client.UpdateItemAsync(updateCommentRequest);
+        }
+
+        public async Task<List<Dictionary<string, AttributeValue>>> GetSortedCommentsForPost(string postId)
+        {
+            QueryRequest queryRequest = new QueryRequest
+            {
+                TableName = TableName,
+                KeyConditionExpression = "PostId = :postId",
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+                {
+                    { ":postId", new AttributeValue { N = postId } }
+                },
+                IndexName = "PostModifiedDateTimeIndex",
+            };
+
+            QueryResponse queryResponse = await _client.QueryAsync(queryRequest);
+
+            return queryResponse.Items;
+        }
+
     }
 }
--- DynamoDb/UIDynamo.cs
+++ DynamoDb/UIDynamo.cs
@@ -1,12 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL;
 
 namespace SocialNetwork.UI
 {
-    internal class UIDynamo
+    public class UIDynamo
     {
+        private readonly SocialNetworkRepositoryDynamo _repository;
+
+        public UIDynamo(SocialNetworkRepositoryDynamo repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Run()
+        {
+            await _repository.CreateSocialMediaTable(); 
+
+            while (true)
+            {
+                PrintMenu();
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        EditPost();
+                        break;
+
+                    case "2":
+                        EditComment();
+                        break;
+
+                    case "3":
+                        GetSortedCommentsForPost();
+                        break;
+
+                    case "4":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a valid option.");
+                        break;
+                }
+
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private void PrintMenu()
+        {
+            Console.WriteLine("1. Edit Post");
+            Console.WriteLine("2. Edit Comment");
+            Console.WriteLine("3. Get Sorted Comments for Post");
+            Console.WriteLine("4. Exit");
+            Console.Write("Choose an option: ");
+        }
+
+        private void EditPost()
+        {
+            Console.Write("Enter the Post ID: ");
+            string postId = Console.ReadLine();
+            Console.Write("Enter the new content for the Post: ");
+            string newPostContent = Console.ReadLine();
+            _repository.EditPost(postId, newPostContent).Wait();
+            Console.WriteLine("Post successfully edited!");
+        }
+
+        private void EditComment()
+        {
+            Console.Write("Enter the Post ID: ");
+            string commentPostId = Console.ReadLine();
+            Console.Write("Enter the Comment ID: ");
+            string commentId = Console.ReadLine();
+            Console.Write("Enter the new Comment text: ");
+            string newCommentText = Console.ReadLine();
+            _repository.EditComment(commentPostId, commentId, newCommentText).Wait();
+            Console.WriteLine("Comment successfully edited!");
+        }
+
+        private async void GetSortedCommentsForPost()
+        {
+            Console.Write("Enter the Post ID: ");
+            string postWithCommentsId = Console.ReadLine();
+            var sortedComments = await _repository.GetSortedCommentsForPost(postWithCommentsId);
+
+            foreach (var comment in sortedComments)
+            {
+                Console.WriteLine($"CommentId: {comment["CommentId"].N}, CommentText: {comment["CommentText"].S}, ModifiedDateTime: {comment["ModifiedDateTime"].S}");
+            }
+        }
     }
 }
